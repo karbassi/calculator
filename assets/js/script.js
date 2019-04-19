b23fd6a4751e3429f370345864485dc8cc13f018
Ali Karbassi <ali@karbassi.com>
2019-04-18 19:05:54 -0500

    JS: Clear button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 var numberElements = document.querySelectorAll(".number");
 var screenElement = document.querySelector(".screen");
+var clearButton = document.querySelector(".clear");
 
 var number = "";
 
+// Number clicked
 function numberClicked(event) {
     var htmlElement = event.target;
     var value = htmlElement.innerText;
@@ -13,6 +15,20 @@ function numberClicked(event) {
     screenElement.innerText = number;
 }
 
+// Add event listeners for all the number buttons
 for (var i = 0; i < numberElements.length; i++) {
     numberElements[i].addEventListener("click", numberClicked);
 }
+
+
+
+// Clear button
+function reset(event) {
+    // set number to empty
+    number = "";
+
+    // clear screen
+    screenElement.innerText = "0";
+}
+
+clearButton.addEventListener("click", reset);
