c796c0023e41b1b97ca00a05694e7281d031d07e
Ali Karbassi <ali@karbassi.com>
2019-04-18 20:58:19 -0500

    JS: Add and equal-add actions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
 var numberElements = document.querySelectorAll(".number");
 var screenElement = document.querySelector(".screen");
 var clearButton = document.querySelector(".clear");
+var addButton = document.querySelector(".plus");
+var equalButton = document.querySelector(".equal");
 
+// number on the screen
 var number = "";
+
+// variables to hold our equation
+var memory1 = null;
+var memory2 = null;
+var action = null;
+
+
+// Update screen
+function updateScreen(number) {
+    screenElement.innerHTML = number;
+}
+
 
 // Number clicked
 function numberClicked(event) {
@@ -12,7 +27,7 @@ function numberClicked(event) {
     number = number + value;
 
     // Update screen
-    screenElement.innerText = number;
+    updateScreen(number);
 }
 
 // Add event listeners for all the number buttons
@@ -24,11 +39,69 @@ for (var i = 0; i < numberElements.length; i++) {
 
 // Clear button
 function reset(event) {
-    // set number to empty
+    // reset all numbers
     number = "";
+    memory1 = null;
+    memory2 = null;
+
 
     // clear screen
-    screenElement.innerText = "0";
+    updateScreen(0);
 }
 
 clearButton.addEventListener("click", reset);
+
+
+// Add button
+function addButtonClicked(event) {
+    console.log("addButtonClicked", memory1, memory2);
+
+    // Save action
+    action = "+";
+
+    if (memory1 == null) {
+        memory1 = parseInt(number, 10);
+    } else {
+        memory2 = parseInt(number, 10);
+    }
+
+    // clear number-holder
+    number = "";
+
+    // if both memory slots are set, then do the math
+    if ((memory1 != null) && (memory2 != null)) {
+
+        // do the math and save it to memory1 slot
+        memory1 = memory1 + memory2;
+
+        // Update screen with the answer/memory1 slot
+        updateScreen(memory1);
+    }
+}
+
+addButton.addEventListener("click", addButtonClicked);
+
+// Equal button
+function equalButtonClicked(event) {
+    console.log("equalButtonClicked", memory1, memory2);
+
+    // if memory1 isn't set, there is no point in doing anything, so just quit
+    if (memory1 == null) {
+        return;
+    }
+
+    // set the last number from the screen to memory2
+    memory2 = parseInt(number, 10);
+
+    if (action == "+") {
+        console.log("equalButtonClicked", memory1, memory2, action);
+
+        // do the math and save it to memory1 slot
+        memory1 = memory1 + memory2;
+
+        // Update screen with the answer/memory1 slot
+        updateScreen(memory1);
+    }
+}
+
+equalButton.addEventListener("click", equalButtonClicked);
