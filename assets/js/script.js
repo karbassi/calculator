f5a29b7e7da9f57bab21f9cf9014cb76a1066ff8
Ali Karbassi <ali@karbassi.com>
2019-04-19 17:32:31 -0500

    Done for now.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,62 @@
+/*
+Not working:
+- If the user presses 1, then plus, then minus, then a 3. It should do 1 - 3,
+  but the second operator is currently just ignored.
+
+- If the user presses 1, plus, 2, equal, plus, equal. It should do 1 + 2 + 2.
+  Currently operator followed by equal is ignored.
+
+*/
+
+
 var numberElements = document.querySelectorAll(".number");
 var screenElement = document.querySelector(".screen");
 var clearButton = document.querySelector(".clear");
-var addButton = document.querySelector(".plus");
-var equalButton = document.querySelector(".equal");
+var operatorButtons = document.querySelectorAll(".operator");
 
 // number on the screen
 var number = "";
 
-// variables to hold our equation
-var memory1 = null;
-var memory2 = null;
-var action = null;
+// Holds all operations
+var stack = [];
 
 
-// Update screen
+
+// ----------------------------------------------------------------------------
+// Helper: Update screen
 function updateScreen(number) {
     screenElement.innerHTML = number;
 }
 
 
+
+function calculate(a, b, operator) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+
+    if (operator == "plus") {
+        return a + b;
+    }
+
+    if (operator == "minus") {
+        return a - b;
+    }
+
+    if (operator == "times") {
+        return a * b;
+    }
+
+    if (operator == "divide") {
+        return a / b;
+    }
+}
+
+
+
+
+// ----------------------------------------------------------------------------
 // Number clicked
-function numberClicked(event) {
+function numberButtonClicked(event) {
     var htmlElement = event.target;
     var value = htmlElement.innerText;
 
@@ -30,78 +66,72 @@ function numberClicked(event) {
     updateScreen(number);
 }
 
-// Add event listeners for all the number buttons
-for (var i = 0; i < numberElements.length; i++) {
-    numberElements[i].addEventListener("click", numberClicked);
+
+
+// Operator Clicked
+function operatorButtonClicked(event) {
+    var htmlElement = event.target;
+    var htmlOperator = htmlElement.value;
+
+    var value = parseFloat(number);
+
+    if (!isNaN(value)) {
+        stack.push(value);
+    }
+
+    number = "";
+
+
+    // If there is enough items in the stack, then let's calculate them and
+    // replace them with the answer.
+    if (stack.length >= 3) {
+        var b = stack.pop();
+        var operator = stack.pop();
+        var a = stack.pop();
+
+        var answer = calculate(a, b, operator);
+
+        stack.push(answer);
+
+        updateScreen(answer);
+    }
+
+    // Get the last element in the stack
+    var last = stack[stack.length - 1];
+
+    // If the operator isn't equal AND
+    // the last element in the stack isn't a number THEN
+    // push the operator to the stack.
+    if (htmlOperator != "equals" && !isNaN(last)) {
+        stack.push(htmlOperator);
+    }
 }
 
 
 
 // Clear button
-function reset(event) {
+function clearButtonClicked(event) {
     // reset all numbers
+    stack = [];
     number = "";
-    memory1 = null;
-    memory2 = null;
-
 
     // clear screen
     updateScreen(0);
 }
 
-clearButton.addEventListener("click", reset);
 
 
-// Add button
-function addButtonClicked(event) {
-    console.log("addButtonClicked", memory1, memory2);
+// ----------------------------------------------------------------------------
+// Event listeners
 
-    // Save action
-    action = "+";
-
-    if (memory1 == null) {
-        memory1 = parseInt(number, 10);
-    } else {
-        memory2 = parseInt(number, 10);
-    }
-
-    // clear number-holder
-    number = "";
-
-    // if both memory slots are set, then do the math
-    if ((memory1 != null) && (memory2 != null)) {
-
-        // do the math and save it to memory1 slot
-        memory1 = memory1 + memory2;
-
-        // Update screen with the answer/memory1 slot
-        updateScreen(memory1);
-    }
+// Add event listeners for all number buttons
+for (var i = 0; i < numberElements.length; i++) {
+    numberElements[i].addEventListener("click", numberButtonClicked);
 }
 
-addButton.addEventListener("click", addButtonClicked);
-
-// Equal button
-function equalButtonClicked(event) {
-    console.log("equalButtonClicked", memory1, memory2);
-
-    // if memory1 isn't set, there is no point in doing anything, so just quit
-    if (memory1 == null) {
-        return;
-    }
-
-    // set the last number from the screen to memory2
-    memory2 = parseInt(number, 10);
-
-    if (action == "+") {
-        console.log("equalButtonClicked", memory1, memory2, action);
-
-        // do the math and save it to memory1 slot
-        memory1 = memory1 + memory2;
-
-        // Update screen with the answer/memory1 slot
-        updateScreen(memory1);
-    }
+// Add event listeners for all operand buttons
+for (var i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener("click", operatorButtonClicked);
 }
 
-equalButton.addEventListener("click", equalButtonClicked);
+clearButton.addEventListener("click", clearButtonClicked);
